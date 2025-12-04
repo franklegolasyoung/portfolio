19ea6cbaf1726afb8e1f48bff7e2e31248a6a91d
franky <frankyoung@outlook.sg>
2025-12-04 11:07:57 +0000

    Update project component to include new features

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -11,7 +11,7 @@ function Project() {
     <div className="projects-container" id="projects">
         <h1>Personal Projects</h1>
         <div className="projects-grid">
-            <div className="project">
+            {/* <div className="project">
                 <a href="https://www.pixcloud.cc/" target="_blank" rel="noreferrer"><img src={pixcloud} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://www.pixcloud.cc/" target="_blank" rel="noreferrer"><h2>PixCloud</h2></a>
                 <p>Developed Image Storage, Sharing, and Cooperation Platform, built with Spring Boot, Vue 3, MySQL, Redis, AWS, COS Storage, CDN Acceleration</p>
@@ -20,7 +20,7 @@ function Project() {
                 <a href="https://www.chatlah.cc" target="_blank" rel="noreferrer"><img src={chatlah} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://www.chatlah.cc" target="_blank" rel="noreferrer"><h2>ChatLah</h2></a>
                 <p>Developed and released a LLM Agent chatbot platform, integrated with multiple MCP providers and personalized RAG knowledge base.</p>
-            </div>
+            </div> */}
             <div className="project">
                 <a href="https://www.semanticscholar.org/paper/CoSPED%3A-Consistent-Soft-Prompt-Targeted-Data-and-Yang-Wai/fb09eaceb17a4813fd8ce3496d01e1c78c6cec06" target="_blank" rel="noreferrer"><img src={cosped} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://www.semanticscholar.org/paper/CoSPED%3A-Consistent-Soft-Prompt-Targeted-Data-and-Yang-Wai/fb09eaceb17a4813fd8ce3496d01e1c78c6cec06" target="_blank" rel="noreferrer"><h2>CoSPED Research Paper</h2></a>
